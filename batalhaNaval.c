abee4fa28f9874b18943317f1df34e78b0b441d7
Ciney55 <walcineysilva3@gmail.com>
2025-05-09 22:07:48 -0300

    crie um jogo de batalha naval em c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,81 @@
-#include <stdio.h>
+# include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAMANHO 10
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+void inicializaTabuleiro(char tabuleiro[TAMANHO][TAMANHO]) {
+    for (int i = 0; i < TAMANHO; i++) {
+            for (int j = 0; j < TAMANHO; j++) {
+                        tabuleiro[i][j] = '~';
+                                }
+                                    }
+                                    }
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+                                    void imprimeTabuleiro(char tabuleiro[TAMANHO][TAMANHO]) {
+                                        printf("  ");
+                                            for (int i = 0; i < TAMANHO; i++) {
+                                                    printf("%d ", i);
+                                                        }
+                                                            printf("\n");
+                                                                for (int i = 0; i < TAMANHO; i++) {
+                                                                        printf("%d ", i);
+                                                                                for (int j = 0; j < TAMANHO; j++) {
+                                                                                            printf("%c ", tabuleiro[i][j]);
+                                                                                                    }
+                                                                                                            printf("\n");
+                                                                                                                }
+                                                                                                                }
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+                                                                                                                void colocaNavios(char tabuleiro[TAMANHO][TAMANHO], int quantidade) {
+                                                                                                                    srand(time(NULL));
+                                                                                                                        for (int i = 0; i < quantidade; i++) {
+                                                                                                                                int linha = rand() % TAMANHO;
+                                                                                                                                        int coluna = rand() % TAMANHO;
+                                                                                                                                                tabuleiro[linha][coluna] = 'N';
+                                                                                                                                                    }
+                                                                                                                                                    } int main() {
+                                                                                                                                                        char tabuleiro[TAMANHO][TAMANHO];
+                                                                                                                                                            char tabuleiroVisivel[TAMANHO][TAMANHO];
+                                                                                                                                                                inicializaTabuleiro(tabuleiro);
+                                                                                                                                                                    inicializaTabuleiro(tabuleiroVisivel);
+                                                                                                                                                                        colocaNavios(tabuleiro, 5);
+                                                                                                                                                                            printf("Batalha Naval!\n");
+                                                                                                                                                                                printf("Tente encontrar os navios!\n");
+                                                                                                                                                                                    int naviosDestruidos = 0;
+                                                                                                                                                                                        while (naviosDestruidos < 5) {
+                                                                                                                                                                                                imprimeTabuleiro(tabuleiroVisivel);
+                                                                                                                                                                                                        int linha, coluna;
+                                                                                                                                                                                                                printf("Digite a linha e a coluna para atacar (separados por espaço): ");
+                                                                                                                                                                                                                        scanf("%d %d", &linha, &coluna);
+                                                                                                                                                                                                                                if (linha < 0 || linha >= TAMANHO || coluna < 0 || coluna >= TAMANHO) {
+                                                                                                                                                                                                                                            printf("Coordenadas inválidas!\n");
+                                                                                                                                                                                                                                                        continue;
+                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                        if (tabuleiro[linha][coluna] == 'N') {
+                                                                                                                                                                                                                                                                                    printf("Navio atingido!\n");
+                                                                                                                                                                                                                                                                                                tabuleiroVisivel[linha][coluna] = 'X';
+                                                                                                                                                                                                                                                                                                            tabuleiro[linha][coluna] = 'X';
+                                                                                                                                                                                                                                                                                                                        naviosDestruidos++;
+                                                                                                                                                                                                                                                                                                                                } else if (tabuleiro[linha][coluna] == '~') {
+                                                                                                                                                                                                                                                                                                                                            printf("Água!\n");
+                                                                                                                                                                                                                                                                                                                                                        tabuleiroVisivel[linha][coluna] = 'O';
+                                                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                                                                                                                                        printf("Você ganhou!\n");
+                                                                                                                                                                                                                                                                                                                                                                            return 0;
+                                                                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                                                            
+                                                                                                                                                                                                                                                                                                                                                                        
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+                                                                                                                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                                                                                                                                            
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+                                                                                                                                                                                                                                                                                                                                                                        
 
-    return 0;
-}
+                                                                                                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                                                                                                                                        
+                                                                                                                            
+
+                                                                                                                                                                                                                                        
